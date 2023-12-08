5fe36bbc614345237849a83646938b98a948ca2c
access <roy.gebrayel11@gmail.com>
2023-12-08 22:36:52 +0200

    added the credentials into the extra in app.config.js

--- connection/connection.js
+++ connection/connection.js
@@ -1,19 +1,21 @@
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import Constants from "expo-constants";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCh4_vxsl8QoWF-KNMAd9nHngvMYe6wRDc",
-  authDomain: "travel-mobile-app-f2687.firebaseapp.com",
-  projectId: "travel-mobile-app-f2687",
-  storageBucket: "travel-mobile-app-f2687.appspot.com",
-  messagingSenderId: "1007492362048",
-  appId: "1:1007492362048:web:3b78306dab5d3c1e44d4a5",
-  measurementId: "G-SF63TVK801",
+  apiKey: Constants.expoConfig.extra.API_URL,
+  authDomain: Constants.expoConfig.extra.AUTH_DOMAIN,
+  projectId: Constants.expoConfig.extra.PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.APP_ID,
+  measurementId: Constants.expoConfig.extra.MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-// Initialize Firebase with AsyncStorage for persistence
+// initialization of the auth service in firebase using the firebase app initialization
+
 export const auth = initializeAuth(firebaseApp, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
